bbbd95702bcb13217592c1dcff4025752eac2db5
Dian Rosanti <dian.rosanti@gmail.com>
2011-06-27 20:43:15 -0700

    added cheater

---
+++ application/views/cheater.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+
+
+       <title>
+               <?php echo $title; ?>
+       </title>
+</head>
+
+<body>
+
+<h1>
+       <?php echo $heading; ?>
+</h1>
+<?php echo $raptick['yourname'];?>, you have already purchased a
+rapture ticket using <?php echo $raptick['email']; ?>. However, you
+are more than welcome to purchase as many raffle tickets as you like!
+</body>
+</html>
